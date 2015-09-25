3ed8f2b57c3470f98d87adda737f64596dc117f6
yuta-t <yutatuy.t@gmail.com>
2015-09-25 16:57:18 +0900

    Initial Commit

---
+++ ShiftViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShiftViewer
+//
+//  Created by YutaTadachi on 2015/09/25.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
