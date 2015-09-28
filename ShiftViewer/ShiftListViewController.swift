b033f7e519e3901edb95c7d803c823335931a3b3
yuta-t <yutatuy.t@gmail.com>
2015-09-28 23:08:48 +0900

    change ShiftListVC

--- ShiftViewer/ShiftListViewController.swift
+++ ShiftViewer/ShiftListViewController.swift
@@ -8,28 +8,36 @@
 
 import UIKit
 
-class ShiftListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class ShiftListViewController: UIViewController, UITableViewDelegate {
+    private let model = ShiftListModel()
+    
+    override func loadView() {
+        view = ShiftListView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let shiftView = view as! ShiftListView
+        
+        shiftView.table.delegate = self
+        shiftView.table.dataSource = model
+        
+        model.getTerms {
+            dispatch_async(dispatch_get_main_queue(), {
+                shiftView.table.reloadData()
+            })
+        }
     }
-    */
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "シフト一覧"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "設定", style: .Plain, target: self, action: "transitToUserSetting:")
+    }
 
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
