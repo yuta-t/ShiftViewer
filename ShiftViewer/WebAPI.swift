750badbf49b04cb4c7aa9c5ff3583f561b741bbb
yuta-t <yutatuy.t@gmail.com>
2015-09-28 23:07:43 +0900

    make WebAPI

---
+++ ShiftViewer/WebAPI.swift
@@ -0,0 +1,21 @@
+//
+//  WebAPI.swift
+//  ShiftViewer
+//
+//  Created by YutaTadachi on 2015/09/28.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class WebAPI {
+    class func jsonObject(url: String, completed: ((AnyObject?) -> Void)) {
+        Alamofire.request(.GET, url)
+            .authenticate(user: "tcamp", password: "master")
+            .responseJSON { (_, _, result) in
+                completed(result.value)
+        }
+    }
+}
