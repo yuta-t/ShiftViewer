3c9ea7d5ee9dce5e06e1bf0a4142837e2692469c
yuta-t <yutatuy.t@gmail.com>
2015-09-29 20:08:27 +0900

    make UserSettingVC

---
+++ ShiftViewer/UserSettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserSettingViewController.swift
+//  ShiftViewer
+//
+//  Created by YutaTadachi on 2015/09/29.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
