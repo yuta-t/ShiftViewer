71fd093b8b1f70a33c339d40352cc8f8a82927aa
yuta-t <yutatuy.t@gmail.com>
2015-09-30 13:12:12 +0900

    make UIColor Extension and add corporate colors

---
+++ ShiftViewer/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  ShiftViewer
+//
+//  Created by YutaTadachi on 2015/09/30.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func corporateRed() -> UIColor {
+        return UIColor(red: 202 / 255, green: 73 / 255, blue: 60 / 255, alpha: 1)
+    }
+    
+    class func corporateWhite() -> UIColor {
+        return UIColor(white: 239 / 255, alpha: 1)
+    }
+}
