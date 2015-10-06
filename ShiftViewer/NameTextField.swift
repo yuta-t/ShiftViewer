1c8e09f17b083f53d2ca9ff1cec9e7a32e4864c1
yuta-t <yutatuy.t@gmail.com>
2015-10-06 20:08:49 +0900

    create TameTextFiled class

---
+++ ShiftViewer/NameTextField.swift
@@ -0,0 +1,12 @@
+//
+//  NameTextField.swift
+//  ShiftViewer
+//
+//  Created by YutaTadachi on 2015/09/30.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import UIKit
+
+class NameTextField: UITextField {
+}
