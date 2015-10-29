e66782c5c11c7c9570e08f4fd023a9a3282b9549
yuta-t <yutatuy.t@gmail.com>
2015-10-30 00:22:40 +0900

    Import SwiftyJSON to operate JSON object
    Add static func from that return Term or throw anything
    
    Change Umeda num in enum Group
    This is not good, because there is a need to change that product code

--- ShiftViewer/Term.swift
+++ ShiftViewer/Term.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct Term {
     enum Group: Int {
-        case Shibuya = 1, Umeda
+        case Shibuya = 1
+        case Umeda = 4
         
         func toStr() -> String {
             switch self {
@@ -20,26 +22,53 @@ struct Term {
         }
     }
     
+    enum InitError: ErrorType {
+        case Unparsable(String)
+        case UnknownGroup
+        case Unconvertable(String)
+    }
+    
     var id: Int
     var group: Group
     var startDate: String
     var finishDate: String
     
-    init(dic: [String: AnyObject]) {
-        self.id = dic["id"] as! Int
-        self.group = Group(rawValue: (dic["mentor_shift_group_id"] as! Int))!
-    
-        let startDate = NSDate.convertFromHyphenSeparatedDate(dic["start_date"] as! String)!
-        self.startDate = startDate.convertToSlashSeparatedDate()
-
-        let finishDate =  NSDate.convertFromHyphenSeparatedDate(dic["finish_date"] as! String)!
-        self.finishDate = finishDate.convertToSlashSeparatedDate()
+    init(id: Int, group: Group, start: String, finish: String) {
+        self.id = id
+        self.group = group
+        startDate = start
+        finishDate = finish
     }
     
-    init(id: Int, groupId: Int, start: String, finish: String) {
-        self.id = id
-        self.group = Group.init(rawValue: groupId)!
-        startDate = NSDate.convert(start)
-        finishDate = NSDate.convert(finish)
+    static func from(json: JSON) throws -> Term {
+        guard let id = json["id"].int else {
+            throw InitError.Unparsable("id")
+        }
+        
+        guard let groupId = json["mentor_shift_group_id"].int else {
+            throw InitError.Unparsable("group id")
+        }
+        
+        guard let group = Group.init(rawValue: groupId) else {
+            throw InitError.UnknownGroup
+        }
+        
+        guard let HyphtnSeparatedStartDate = json["start_date"].string else {
+            throw InitError.Unparsable("start date")
+        }
+        
+        guard let start = NSDate.convert(HyphtnSeparatedStartDate) else {
+            throw InitError.Unconvertable("start date")
+        }
+        
+        guard let HyphtnSeparatedFinishDate = json["finish_date"].string else {
+            throw InitError.Unparsable("finish date")
+        }
+        
+        guard let finish = NSDate.convert(HyphtnSeparatedFinishDate) else {
+            throw InitError.Unconvertable("finish date")
+        }
+        
+        return self.init(id: id, group: group, start: start, finish: finish)
     }
 }
