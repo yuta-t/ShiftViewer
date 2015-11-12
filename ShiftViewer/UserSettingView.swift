e639373d39b75650080ec819d7021bd1f8fd8f7c
yuta-t <yutatuy.t@gmail.com>
2015-11-12 15:15:30 +0900

    Create UserSettingView by xib

--- ShiftViewer/UserSettingView.swift
+++ ShiftViewer/UserSettingView.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class UserSettingView: UIView {
-    
+    @IBOutlet weak var familyNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundColor = UIColor.whiteColor()
+        
+        sendButton.setTitleColor(UIColor.corporateRed(), forState:.Normal)
+        
+        layoutTextField(familyNameTextField)
+        layoutFamilyNameTextField(familyNameTextField)
+        
+        layoutTextField(firstNameTextField)
+        layoutFirstNameTextField(firstNameTextField)
     }
     
+    func layoutTextField(textField: UITextField) {
+        textField.borderStyle = .None
+        textField.textAlignment = .Center
+        textField.textColor = UIColor.corporateGray()
+        
+        let border = CALayer()
+        let width = CGFloat(1)
+        border.borderColor = UIColor.corporateRed().CGColor
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - width, width:  textField.frame.size.width, height: textField.frame.size.height)
+        
+        border.borderWidth = width
+        textField.layer.addSublayer(border)
+        textField.layer.masksToBounds = true
+    }
+    
+    func layoutFamilyNameTextField(textField: UITextField) {
+
+    }
+    
+    func layoutFirstNameTextField(textField: UITextField) {
+        
+    }
 }
