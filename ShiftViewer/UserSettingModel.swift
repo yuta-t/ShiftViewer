a72b6f56fe9e814ba264f4d4c8668cb31fd79a0d
yuta-t <yutatuy.t@gmail.com>
2015-11-18 19:28:30 +0900

    Make model of UserSetting

---
+++ ShiftViewer/UserSettingModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserSettingModel.swift
+//  ShiftViewer
+//
+//  Created by Yuta Tadachi on 2015/11/18.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import Foundation
+
+final class UserSettingModel: NSObject {
+    
+}
